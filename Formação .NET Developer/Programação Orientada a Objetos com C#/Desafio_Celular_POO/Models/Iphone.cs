33ac0e8fcfc4dd6e7e08747b15f3b365bc63262f
Paulo Henrique Graciano <paulociano@gmail.com>
2023-01-21 01:44:20 -0300

    Adiciona Projeto POO

---
+++ Formação .NET Developer/Programação Orientada a Objetos com C#/Desafio_Celular_POO/Models/Iphone.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_Celular_POO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no iPhone");
+        }
+    }
+}
